6d5667ca960f58d54dadfcb07ee092a3e479667a
ZakyMalika <zakymalika0987@gmail.com>
2025-12-20 00:10:51 +0700

    membuat dan menambah PetaNavigasi.kt

---
+++ app/src/main/java/com/example/mysqlfirst/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,40 @@
+package com.example.mysqlfirst.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mysqlfirst.uicontroller.route.DestinasiEntry
+import com.example.mysqlfirst.uicontroller.route.DestinasiHome
+import com.example.mysqlfirst.view.EntrySiswaScreen
+import com.example.mysqlfirst.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier
+){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+//                    navController.navigate("${DestinasiDetail.route}/${it}")
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome
+                .route) })
+        }
+    }
+}
