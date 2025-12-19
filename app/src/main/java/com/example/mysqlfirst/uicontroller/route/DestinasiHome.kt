bcd3484c1915907981db6ad3d58da2304bbad66d
ZakyMalika <zakymalika0987@gmail.com>
2025-12-19 23:34:22 +0700

    membuat dan menambahkan destinasi home

---
+++ app/src/main/java/com/example/mysqlfirst/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.mysqlfirst.uicontroller.route
+
+import com.example.mysqlfirst.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
