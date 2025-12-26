4d2b8e2dd42570eee0a3b2564250d81dd2626770
ZakyMalika <zakymalika0987@gmail.com>
2025-12-27 01:55:28 +0700

    merubah isi dari RepositoryDataSiswa.kt

--- app/src/main/java/com/example/mysqlfirst/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/mysqlfirst/repositori/RepositoryDataSiswa.kt
@@ -6,6 +6,11 @@ import com.example.mysqlfirst.modeldata.DataSiswa
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
+
 }
 
 class JaringanRepositoryDataSiswa(
@@ -13,4 +18,7 @@ class JaringanRepositoryDataSiswa(
 ):RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
