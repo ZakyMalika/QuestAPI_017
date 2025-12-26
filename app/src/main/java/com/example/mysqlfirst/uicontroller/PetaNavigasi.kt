9336bf861a17812304cee35bce73e61aa262e727
ZakyMalika <zakymalika0987@gmail.com>
2025-12-27 06:07:34 +0700

    perubahan

--- app/src/main/java/com/example/mysqlfirst/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/mysqlfirst/uicontroller/PetaNavigasi.kt
@@ -35,20 +35,20 @@ fun HostNavigasi(
                     navController.navigate("${DestinasiDetail.route}/${it}")})
         }
         composable(DestinasiEntry.route){
-            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
         }
         composable(DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail
             .itemIdArg) {
             type = NavType.IntType })
         ){
             DetailSiswaScreen(navigateToEditItem = { navController.navigate("${DestinasiEdit.route}/$it")},
-                navigateBack = { navController.navigate(DestinasiHome.route) })
+                navigateBack = { navController.popBackStack() })
         }
         composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg
         ){
             type= NavType. IntType})) {
             EditSiswaScreen(
-                navigateBack = { navController.navigate(DestinasiHome.route) },
+                navigateBack = { navController.popBackStack() },
                 onNavigateUp = { navController.navigateUp() })
         }
     }
