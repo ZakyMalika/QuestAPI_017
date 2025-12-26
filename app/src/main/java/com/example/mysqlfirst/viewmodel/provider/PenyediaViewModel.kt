3dae7e5af2afd5ae362413e13d2d2e0ccd99f755
ZakyMalika <zakymalika0987@gmail.com>
2025-12-27 05:07:38 +0700

    perbaikan viewmodel

--- app/src/main/java/com/example/mysqlfirst/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mysqlfirst/viewmodel/provider/PenyediaViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.mysqlfirst.viewmodel.provider
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
@@ -13,23 +12,22 @@ import com.example.mysqlfirst.viewmodel.EntryViewModel
 import com.example.mysqlfirst.viewmodel.HomeViewModel
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+                AplikasiDataSiswa
         )
-object PenyediaViewModel {
+object PenyediaViewModel{
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer {
-            DetailViewModel(
-                this.createSavedStateHandle(),
-                aplikasiDataSiswa().container.repositoryDataSiswa
-            )
+            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
         }
         initializer {
-            EditViewModel(
-                this.createSavedStateHandle(),
-                aplikasiDataSiswa().container.repositoryDataSiswa
-            )
+            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            EditViewModel(this.createSavedStateHandle(),aplikasiDataSiswa().container.repositoryDataSiswa)
         }
     }
 }
