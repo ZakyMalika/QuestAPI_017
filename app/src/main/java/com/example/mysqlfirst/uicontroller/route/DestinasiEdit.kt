b85e436a0f65c191e771fa6bd3145e6a3f14426a
ZakyMalika <zakymalika0987@gmail.com>
2025-12-27 01:50:06 +0700

    merubah isi dari DestinasiEdit.kt.kt.kt

--- app/src/main/java/com/example/mysqlfirst/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/mysqlfirst/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,10 @@
 package com.example.mysqlfirst.uicontroller.route
 
+import com.example.mysqlfirst.R
+
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
